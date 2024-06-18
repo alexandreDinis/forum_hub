3ca4e7750080ce6d32a3186e4f4f717cf1b16572
dinis <dinisde@gmail.com>
2024-06-18 19:56:44 -0300

    iniciando projeto

---
+++ src/test/java/com/forum/hub/ForumApi/ForumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.hub.ForumApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
